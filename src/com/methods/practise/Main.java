dc450e81ea5aa5c7ee7aa4d406e417d394f54799
Douglas Currier <creationmachine66@gmail.com>
2022-04-27 10:20:58 -0400

    first attempt

--- src/com/methods/practise/Main.java
+++ src/com/methods/practise/Main.java
@@ -3,6 +3,6 @@ package com.methods.practise;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("test to see if this pushes to get hub");
     }
 }
